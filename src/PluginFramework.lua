11160b0ea290c3873637c3961f57d7464442791c
Twin1Twin2 <theawesometwin827@gmail.com>
2017-09-14 02:08:02 -0700

    Input Events enable/disable on plugin enable/disable

--- src/PluginFramework.lua
+++ src/PluginFramework.lua
@@ -80,14 +80,124 @@ end
 function PluginFramework:SetPluginActive(value)
     if (value == true) then
         if (self.IsOn == false) then
-            self.TurnedOn = true
+            self.TurnedOn = true        --prevent deactivation when activate is called
             self.Plugin:Activate(true)
-            self.TurnedOn = false
+            self.TurnedOn = false       --
 
             self.IsOn = true
         end
     else
-        self.Plugin:Activate(false)
+        self.Plugin:Activate(false)     --wll trigger the plugin deactivation event
+    end
+end
+
+
+function PluginFramework:EnableInput()
+    local leftCtrl  = false
+    local rightCtrl = false
+    local leftAlt   = false
+    local rightAlt  = false
+
+    local function UpdateCtrl()
+        if (leftCtrl == true or rightCtrl == true) then
+            self.CtrlIsDown = true
+        else
+            self.CtrlIsDown = false
+        end
+    end
+
+    local function UpdateAlt()
+        if (leftAlt == true or rightAlt == true) then
+            self.AltIsDown = true
+        else
+            self.AltIsDown = false
+        end
+    end
+    
+    self.Button2DownConnection  = self.Mouse.Button2Down:Connect(function()
+        self.MouseButton2IsDown = true
+    end)
+
+    self.Button2UpConnection    = self.Mouse.Button2Up:Connect(function()
+        self.MouseButton2IsDown = false
+    end)
+
+    self.InputBeganConnection   = UserInputService.InputBegan:Connect(function(inputObject, gameProcessedEvent)
+        local keyCode = inputObject.KeyCode
+
+        if (keyCode == Enum.KeyCode.LeftControl) then
+            leftCtrl    = true
+            UpdateCtrl()
+        elseif (keyCode == Enum.KeyCode.RightControl) then
+            rightCtrl   = true
+            UpdateCtrl()
+        elseif (keyCode == Enum.KeyCode.LeftAlt) then
+            leftAlt     = true
+            UpdateCtrl()
+        elseif (keyCode == Enum.KeyCode.RightAlt) then
+            rightAlt    = true
+            UpdateCtrl()
+        end
+
+        --KeyDown:Fire(keyCode)
+    end)
+
+    self.InputEndedConnection   = UserInputService.InputEnded:Connect(function(inputObject, gameProcessedEvent)
+        local keyCode = inputObject.KeyCode
+
+        if (keyCode == Enum.KeyCode.LeftControl) then
+            leftCtrl    = false
+            UpdateCtrl()
+        elseif (keyCode == Enum.KeyCode.RightControl) then
+            rightCtrl   = false
+            UpdateCtrl()
+        elseif (keyCode == Enum.KeyCode.LeftAlt) then
+            leftAlt     = false
+            UpdateCtrl()
+        elseif (keyCode == Enum.KeyCode.RightAlt) then
+            rightAlt    = false
+            UpdateCtrl()
+        end
+
+        --KeyDown:Fire(keyCode)
+    end)
+
+    self.WindowFocusedReleasedConnection    = UserInputService.WindowFocusedReleased:Connect(function()
+        self.CtrlIsDown         = false
+        self.AltIsDown          = false
+        self.MouseButton2IsDown = false
+    end)
+end
+
+
+function PluginFramework:DisableInput()
+    self.CtrlIsDown         = false
+    self.AltIsDown          = false
+    self.MouseButton2IsDown = false
+
+    if (self.Button2DownConnection ~= nil) then
+        self.Button2DownConnection:Disconnect()
+        self.Button2DownConnection  = nil
+    end
+
+    if (self.Button2UpConnection ~= nil) then
+        self.Button2UpConnection:Disconnect()
+        self.Button2UpConnection    = nil
+    end
+
+    if (self.InputBeganConnection ~= nil) then
+        self.InputBeganConnection:Disconnect()
+        self.InputBeganConnection   = nil
+    end
+
+    if (self.InputEndedConnection ~= nil) then
+        self.InputEndedConnection:Disconnect()
+        self.InputEndedConnection   = nil
+    end
+
+    if (self.WindowFocusedReleasedConnection ~= nil) then
+        self.WindowFocusedReleasedConnection:Disconnect()
+        self.WindowFocusedReleasedConnection    = nil
     end
 end
 
@@ -112,11 +222,15 @@ function PluginFramework:new(pluginName)
         IsOn        = false;
         TurnedOn    = false;
 
-
-
         CtrlIsDown          = false;
         AltIsDown           = false;
         MouseButton2IsDown  = false;
+
+        Button2DownConnection           = nil;
+        Button2UpConnection             = nil;
+        InputBeganConnection            = nil;
+        InputEndedConnection            = nil;
+        WindowFocusedReleasedConnection = nil;
     }
 
 
@@ -127,90 +241,15 @@ function PluginFramework:new(pluginName)
     local mouse     = plugin:GetMouse()
 
 
-    mouse.Button2Down:Connect(function()
-        this.MouseButton2IsDown = true
-    end)
-
-    mouse.Button2Up:Connect(function()
-        this.MouseButton2IsDown = false
-    end)
-
-    local leftCtrl  = false
-    local rightCtrl = false
-    local leftAlt   = false
-    local rightAlt  = false
-
-    local function UpdateCtrl()
-        if (leftCtrl == true or rightCtrl == true) then
-            this.CtrlIsDown = true
-        else
-            this.CtrlIsDown = false
-        end
-    end
-
-    local function UpdateAlt()
-        if (leftAlt == true or rightAlt == true) then
-            this.AltIsDown = true
-        else
-            this.AltIsDown = false
-        end
-    end
-
-    UserInputService.InputBegan:Connect(function(inputObject, gameProcessedEvent)
-        local keyCode = inputObject.KeyCode
-
-        if (keyCode == Enum.KeyCode.LeftControl) then
-            leftCtrl    = true
-            UpdateCtrl()
-        elseif (keyCode == Enum.KeyCode.RightControl) then
-            rightCtrl   = true
-            UpdateCtrl()
-        elseif (keyCode == Enum.KeyCode.LeftAlt) then
-            leftAlt     = true
-            UpdateCtrl()
-        elseif (keyCode == Enum.KeyCode.RightAlt) then
-            rightAlt    = true
-            UpdateCtrl()
-        end
-
-        --KeyDown:Fire(keyCode)
-    end)
-
-    UserInputService.InputEnded:Connect(function(inputObject, gameProcessedEvent)
-        local keyCode = inputObject.KeyCode
-
-        if (keyCode == Enum.KeyCode.LeftControl) then
-            leftCtrl    = false
-            UpdateCtrl()
-        elseif (keyCode == Enum.KeyCode.RightControl) then
-            rightCtrl   = false
-            UpdateCtrl()
-        elseif (keyCode == Enum.KeyCode.LeftAlt) then
-            leftAlt     = false
-            UpdateCtrl()
-        elseif (keyCode == Enum.KeyCode.RightAlt) then
-            rightAlt    = false
-            UpdateCtrl()
-        end
-
-        --KeyDown:Fire(keyCode)
-    end)
+    this.Plugin     = plugin
+    this.Mouse      = mouse
 
 
-    UserInputService.WindowFocusedReleased:Connect(function()
-        this.CtrlIsDown         = false
-        this.AltIsDown          = false
-        this.MouseButton2IsDown = false
-    end)
-
-
-    plugin.Deactivation:Connect(function()
+    this.Plugin.Deactivation:Connect(function()
         if (this.IsOn == true and this.TurnedOn == false) then
             this:ClearInputEvents()
 
-            this.CtrlIsDown         = false
-            this.AltIsDown          = false
-            this.MouseButton2IsDown = false
+            this:DisableInput()
 
             this:DeselectTool(this.SelectedTool)
             this.PreviousTool       = this.SelectedTool
@@ -219,11 +258,6 @@ function PluginFramework:new(pluginName)
             this.IsOn = false
         end
     end)
-
-
-    this.Plugin     = plugin
-    this.Mouse      = mouse
-
     
     return this
 end
