f6d89f0a020f2d8769536cc7b769a188ba9977b8
Twin1Twin2 <theawesometwin827@gmail.com>
2017-09-14 22:33:10 -0700

    Extended MenuButton.lua

--- src/modules/MenuButton.lua
+++ src/modules/MenuButton.lua
@@ -15,23 +15,36 @@ local MenuObjeect   = require(script.Parent.MenuObject)
 -- // MAIN CODE // --
 
 local MenuButton    = {
-
+    ClassName   = "MenuButton";
 }
 
 
-function MenuButton:new(pluginManager, name)
+function MenuButton:new(menu, name)
+    local pluginManager = menu.PluginManager
+
     local base  = MenuObject:new(pluginManager, name)
 
     base.__index    = base
     setmetatable(self, base)
 
     local this  = {
+        Icon    = nil;
+        Color   = Color3.new(0, 0, 0);
 
+        OnMouseButton1Click = nil;  --Signal
+        OnMouseButton2Click = nil;  --Signal
+
+        OnCtrlClick         = nil;  --Signal
+        OnAltClick          = nil;  --Signal
+        OnCtrlAltClick      = nil;  --Signal
+
+        OnDisabledChanged   = nil;  --Signal
     }
 
 
     self.__index    = self
     setmetatable(this, self)
+
 
     return this
 end
