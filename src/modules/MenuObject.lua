b3ebc86e8d77a38beaf0a12b88bee426a489f6a7
Twin1Twin2 <theawesometwin827@gmail.com>
2017-09-14 22:32:33 -0700

    Expanded MenuObject.lua

--- src/modules/MenuObject.lua
+++ src/modules/MenuObject.lua
@@ -17,14 +17,80 @@ local MenuObject    = {
 }
 
 
-function MenuObject:new()
-    local this  = {
+function MenuObject:SetVisible(value)
+    if (value ~= nil and type(value) ~= "boolean") then
+        error("Argument [1]", 2)
+    end
 
+    if (value ~= nil) then
+        if (self.Visible == value) then
+            return
+        end
+        self.Visible    = value
+    else
+        self.Visible    = not self.Visible
+    end
+    
+    self.OnVisibleChanged:Fire(self.Visible)
+end
+
+
+function MenuObject:SetEnabled(value)
+    if (value ~= nil and type(value) ~= "boolean") then
+        error("Argument [1]", 2)
+    end
+
+    if (value ~= nil) then
+        if (self.Enabled == value) then
+            return
+        end
+        self.Enabled    = value
+    else
+        self.Enabled    = not self.Enabled
+    end
+
+    self.OnEnabledChanged:Fire(self.Enabled)
+end
+
+
+function MenuObject:new(pluginManager, name)
+    local this  = {
+        Name    = "";
+
+        PluginManager   = nil;
+
+        Visible     = true;
+        Enabled     = true;
+
+        Text            = "";
+        MouseOverText   = "";
+
+        CustomStylist   = nil;
+
+        Position        = nil;
+
+        OnVisibleChanged    = nil;  --Signal
+        OnEnabledChanged    = nil;  --Signal
+
+        Stackable           = true;
+
+        BaseFrameStylist    = nil;
+
+        GUIObjects  = {};
     }
 
 
     self.__index    = self
     setmetatable(this, self)
+
+
+    this.PluginManager  = pluginManager
+
+    this.Name   = name
+
+
+    this.OnVisibleChanged   = Signal:new()
+    this.OnEnabledChanged   = Signal:new()
 
     
     return this
